98e13b239e108deadbb3575f4916c0de50fecaeb
Sourabh Kumar <155216316+Sourabh-Kumar04@users.noreply.github.com>
2024-06-16 20:48:58 +0000

    Add Operations in 03_ConversionOperation.js

--- Web Development/Javascript/01 Basic/03_ConversionOperation.js
+++ Web Development/Javascript/01 Basic/03_ConversionOperation.js
@@ -5,12 +5,12 @@
 // let score = true
 let score = "Raso"
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 let valueNumber = Number(score)
-console.log(typeof valueNumber);
-console.log(valueNumber)
+// console.log(typeof valueNumber);
+// console.log(valueNumber)
 
 /*
     "33" => 33
@@ -25,7 +25,7 @@ console.log(valueNumber)
 let isLoggedIn = "Raso" 
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
     
 /*
     1 => true ; 0 => false
@@ -36,5 +36,46 @@ console.log(booleanIsLoggedIn);
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// *********** Operations *************
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 2);
+// console.log(2 / 2);
+// console.log(2 % 2);
+
+let str1 = "Hello"
+let str2 = " Raso"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");  // for referto : https://tc39.es/ecma262/#sec-abstract-operations
+
+// console.log( ((3 + 4) * 5) % 4);
+
+// console.log(true) //true
+// console.log(+true); // 1
+// console.log(true+); // error
+// console.log(+""); // 0
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+++gameCounter;
+console.log(gameCounter);
