39787067419d45b52bc505528e8a0a90c82003af
Sourabh-Kumar04 <sourabhkumar01010101@gmail.com>
2024-06-20 19:38:01 +0530

    add scope level and mini hosting

--- Web Development/Javascript/03_basics/02_scopes.js
+++ Web Development/Javascript/03_basics/02_scopes.js
@@ -1,22 +1,57 @@
 // var c = 300
 let a = 300   // global scope
 if (true) {
-    let a = 10    //block scope
-    const b = 20   // block scope
+    let a = 10    // block/local scope
+    const b = 20   // local/block scope
     // var c = 30
     // c = 30
-    console.log("INNER: ", a);
+    // console.log("INNER: ", a);
 }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
 
+// **scope is different in browser and node**
 
-// scope is different in browser and node
+// neseted scope
+function one () {
+    const username = "raso"
+
+    function two () {
+        const website = "youtube"
+        console.log(username);
+        console.log(website);
+    }
+    // console.log(website);  //error
+
+    two()
+}
+
+// one()
+
+if (true) {
+    const username = "raso"
+    if (username === "raso") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website); // error 
+}
+// console.log(username);   // error
 
 
+// +++++++++++++++++++++++++ interesting +++++++++++++++++++
 
+addOne(5)  // can run before intialisation
+function addOne (num) {
+    return num + 1
+}
+// addOne(5)
 
-
+// addTwo(5)  // Error: cannot run before initialisation
+const addTwo = function(num) {    // this is func. but sometimes it is called expression
+    return num + 2
+}
+addTwo(5)
 
