a43869af9f8b0cced03e88faa4200aa923d4ebe6
Sourabh-Kumar04 <sourabhkumar01010101@gmail.com>
2024-06-23 00:03:08 +0530

    add dowhile loop

--- Web Development/Javascript/05_iteration/02_while_loop.js
+++ Web Development/Javascript/05_iteration/02_while_loop.js
@@ -10,10 +10,20 @@ let myArray = ['flash', 'batman', 'superman']
 
 let arr = 0
 while (arr < myArray.length) {
-    console.log(`Value of ${myArray[arr]}`);
+    // console.log(`Value of ${myArray[arr]}`);
     arr = arr + 1
 }
 
+
+// while loop
+
+// let score = 1
+let score = 11
+
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
 
 
 
