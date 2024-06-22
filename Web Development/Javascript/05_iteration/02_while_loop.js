0fdcff25ad63ff725fc2b962b9b062c17a861e37
Sourabh-Kumar04 <sourabhkumar01010101@gmail.com>
2024-06-22 23:58:15 +0530

    add while loop

---
+++ Web Development/Javascript/05_iteration/02_while_loop.js
@@ -0,0 +1,26 @@
+// while loop
+
+let index = 0
+while (index <= 10) {
+    // console.log(`Value of index is ${index}`);
+    index = index + 2
+}
+
+let myArray = ['flash', 'batman', 'superman'] 
+
+let arr = 0
+while (arr < myArray.length) {
+    console.log(`Value of ${myArray[arr]}`);
+    arr = arr + 1
+}
+
+
+
+
+
+
+
+
+
+
+
